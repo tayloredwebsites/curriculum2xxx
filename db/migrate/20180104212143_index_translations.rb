a5c926f55f580d2a4ba8d48c39532c6420c98e6d
David Taylor <tayloredwebsites@me.com>
2018-03-04 17:27:00 -0500

    fix limit index length on mysql text field and remove combined index

--- db/migrate/20180104212143_index_translations.rb
+++ db/migrate/20180104212143_index_translations.rb
@@ -1,11 +1,9 @@
 class IndexTranslations < ActiveRecord::Migration[5.1]
   def self.up
-    add_index :translations, [:locale, :key], name: 'index_translations_on_keys'
-    add_index :translations, :value, length: 10
+    add_index :translations, :value, length: 255
 
   end
   def self.down
-    remove_index :translations, name: 'index_translations_on_keys'
     remove_index :translations, :column => :value
   end
 end
