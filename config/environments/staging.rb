1d24d81d5f48fabbddf6c5b7901a9d39b0e158fe
David Taylor <tayloredwebsites@me.com>
2018-03-19 16:05:49 -0400

    config.i18n.fallbacks working in staging and production

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -72,9 +72,9 @@ Rails.application.configure do
   # To Do: set default url option for devise.
   # config.action_mailer.default_url_options = { host: 'thedomain.edu', port: 80 }
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  # # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
+  # # the I18n.default_locale when a translation cannot be found).
+  # config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
