a62eecf46c31ad7d5b151d3b64aef6825454ffe7
David Taylor <tayloredwebsites@me.com>
2018-03-19 17:45:31 -0400

    Tests for cleanups done.

--- .simplecov
+++ .simplecov
@@ -1,5 +1,9 @@
 # .simplecov
+
+# allow 1 hour between all tests that need to be combined
 SimpleCov.merge_timeout 3600
+
+# allow simplecov to be called only once when used in multiple tests
 SimpleCov.start 'rails' do
   # any custom configs like groups and filters can be here at a central place
 end
--- test/system/trees_system_test.rb
+++ test/system/trees_system_test.rb
@@ -160,6 +160,7 @@ class TreesSystemTest < ApplicationSystemTestCase
     assert_equal(trees_path('en'), current_path)
     # at trees index page
     assert_equal I18n.translate('trees.index.name', locale: 'en'), page.title
+    select('Hem', from: 'tree_subject_id')
     # list all grade levels (9 & 13)
     page.find("form.new_tree input[type='submit']").click
     assert_equal(index_listing_trees_path('en'), current_path)
@@ -169,7 +170,7 @@ class TreesSystemTest < ApplicationSystemTestCase
     within('.area-row') { assert page.has_content?('Area 1: MATTER'), 'missing matching area row' }
     within('.component-row') { assert page.has_content?('Component 1: Structure and property of matter'), 'missing matching component row' }
     within('.outcome-row') { assert page.has_content?('Outcome 1: differentiates composition and type of matter'), 'missing matching outcome row' }
-    within('.indicator-name') { assert page.has_content?("1.1.1.a: Indicator a:"), 'missing indicator' }
+    within('.indicator-name') { assert page.has_content?("Indicator 1.1.1.a:"), 'missing indicator' }
     assert_equal 'Knowing properties of materials is a basis for modern production of materials', page.find('.rel-reason-col.val').text
     within('.rel-sector-col.val') do
       assert page.has_content?("3 - Technology of materials")
