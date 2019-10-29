9f873d90e6f83d6c6587b294654d49539506c910
David Taylor <tayloredwebsites@me.com>
2019-10-29 17:40:05 -0400

    Font Size Adjuster in Header

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -40,6 +40,56 @@ $(function() {
 
 
   //###################################
+  //# ADD EVENT BINDINGS AND HANDLERS TOGETHER
+
+
+  $("#pageHeaderFontSize .smallest-text").on("click", function(event, state) {
+    $("#outer-container").addClass("smallest-text");
+    $("#outer-container").removeClass("smaller-text");
+    $("#outer-container").removeClass("medium-text");
+    $("#outer-container").removeClass("larger-text");
+    $("#outer-container").removeClass("largest-text");
+  })
+
+
+  $("#pageHeaderFontSize .smaller-text").on("click", function(event, state) {
+    $("#outer-container").removeClass("smallest-text");
+    $("#outer-container").addClass("smaller-text");
+    $("#outer-container").removeClass("medium-text");
+    $("#outer-container").removeClass("larger-text");
+    $("#outer-container").removeClass("largest-text");
+  })
+
+
+  $("#pageHeaderFontSize .medium-text").on("click", function(event, state) {
+    $("#outer-container").removeClass("smallest-text");
+    $("#outer-container").removeClass("smaller-text");
+    $("#outer-container").addClass("medium-text");
+    $("#outer-container").removeClass("larger-text");
+    $("#outer-container").removeClass("largest-text");
+  })
+
+
+  $("#pageHeaderFontSize .larger-text").on("click", function(event, state) {
+    $("#outer-container").removeClass("smallest-text");
+    $("#outer-container").removeClass("smaller-text");
+    $("#outer-container").removeClass("medium-text");
+    $("#outer-container").addClass("larger-text");
+    $("#outer-container").removeClass("largest-text");
+  })
+
+
+  $("#pageHeaderFontSize .largest-text").on("click", function(event, state) {
+    $("#outer-container").removeClass("smallest-text");
+    $("#outer-container").removeClass("smaller-text");
+    $("#outer-container").removeClass("medium-text");
+    $("#outer-container").removeClass("larger-text");
+    $("#outer-container").addClass("largest-text");
+  })
+
+
+
+  //###################################
   //# ADD EVENT BINDINGS
 
   $(".fa-bars").on('click', function(event, state) {
