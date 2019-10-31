de959a01498af16d9e8631e6815ace6545527a10
esopp <emily@Tonys-MacBook-Pro-3.local>
2019-10-31 16:02:41 -0400

    working popup modal (UI side, but not server side) for making LO connections

--- app/assets/javascripts/sequence.js
+++ app/assets/javascripts/sequence.js
@@ -52,6 +52,10 @@ $(function() {
  
   }
 
+  makeConnection = function () {
+     $("#modal_popup").modal('hide');
+  }
+
 
 });
 
@@ -102,14 +106,70 @@ initializeSortAndDrag = function () {
       // el_under_mouse.closest('.list-group-item').className += 'highlight';
      },
      stop: function (e,ui) {
-      console.log(e)
+      console.log(ui)
       var el_under_mouse = document.elementFromPoint(e.clientX, e.clientY);
       //closest() returns null if no parent found with selector
-      el_under_mouse.closest('.list-group-item').style.background = 'blue';
+      var lo_to_connect = el_under_mouse.closest('.list-group-item')
+      if (lo_to_connect != null) {
+        lo_to_connect = lo_to_connect.id.split('_')[1];
+        lo_being_dragged = ui.helper[0].dataset['loId']
+        console.log('under mouse', lo_to_connect, 'original obj',  lo_being_dragged);
+        $("#modal_popup").modal('show');
+        $.ajax({
+          "type": 'get', 
+          "url": '/tree_trees/new', 
+          "data": {
+            "source_controller": 'tree_trees',
+            "source_action": 'new',
+            "tree_tree[tree_referencer_id]": lo_being_dragged,
+            "tree_tree[tree_referencee_id]": lo_to_connect
+          },
+          "dataType": 'json',
+          "async": false
+        })
+        .then(function (res) { 
+          console.log("RESPONSE:", res) 
+          html = '<div class="modal-header"> \
+                  <h3 id="myModalLabel">LO Connection</h3> \
+                  </div> \
+                  <div class="modal-body"> \
+                    <div> \
+                      <input type="hidden" name="tree_tree[tree_referencer_id]" value=' + res.tree_tree.tree_referencer_id +'> \
+                      <input type="hidden" name="tree_tree[tree_referencee_id]" value=' + res.tree_tree.tree_referencee_id + '> \
+                    </div> \
+                    <fieldset> \
+                    <label for="relationship">' + res.translations.relationship + '</label><br>'
+                    + res.referencer_code + '<br> \
+                    <select id="relationship" name="tree_tree[relationship]"> \
+                    <option value="' + res.relation_values.applies + '">'
+                    + res.translations.applies 
+                    + '</option> \
+                    <option value="' + res.relation_values.depends + '">'
+                    + res.translations.depends 
+                    + '</option> \
+                    <option value="' + res.relation_values.akin + '">'
+                    + res.translations.akin 
+                    + '</option> \
+                    </select>\
+                    </fieldset> \
+                    <br><div>' + res.referencee_code + '</div><br> \
+                    <fieldset>\
+                      <label for="explanation">' + res.translations.explanation + ' \
+                      <input type="text"> \
+                    </fieldset>\
+                    <button onclick="makeConnection()" >SAVE</button>\
+                    </div> \
+                  '
+          $("#modal-container").html(html)
+        })
+        .catch(function (err) { console.log("ERROR:", err) })
 
+      }
      }
    })
 }
+
+
 
 $(document).on('turbolinks:load', function(event, state) {
  initializeSortAndDrag();
--- app/models/tree_tree.rb
+++ app/models/tree_tree.rb
@@ -1,4 +1,16 @@
 class TreeTree < BaseRec
   belongs_to :tree_referencer, class_name: 'Tree'
   belongs_to :tree_referencee, class_name: 'Tree'
+
+  AKIN_KEY = 'akin'
+  APPLIES_KEY = 'applies'
+  DEPENDS_KEY = 'depends'
+  recip_lookup = Hash.new 
+  recip_lookup[AKIN_KEY] = AKIN_KEY
+  recip_lookup[APPLIES_KEY] = DEPENDS_KEY
+  recip_lookup[DEPENDS_KEY] = APPLIES_KEY
+
+  def reciprocal_relationship(relation)
+  	recip_lookup[relation]
+  end
 end
