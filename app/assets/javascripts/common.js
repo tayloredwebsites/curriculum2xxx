4d7349ded6970e9db6e006518cc7f725dc13f51c
esopp <esoppenheim@gmail.com>
2020-04-13 19:50:03 -0400

    Delete unused/breaking migration and models for ckeditor. Fix javascript errors that caused bugs in production.

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -229,10 +229,12 @@ $(function() {
   $.fn.modal.Constructor.prototype._enforceFocus = function() {
   modal_this = this
   $(document).on('focusin.modal', function (e) {
-    if (modal_this.$element[0] !== e.target && !modal_this.$element.has(e.target).length
-    && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select')
-    && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_text')) {
-      modal_this.$element.focus()
+    if (modal_this.$element[0]) {
+      if (modal_this.$element[0] !== e.target && !modal_this.$element.has(e.target).length
+      && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_select')
+      && !$(e.target.parentNode).hasClass('cke_dialog_ui_input_text')) {
+        modal_this.$element.focus()
+      }
     }
   })
   };
