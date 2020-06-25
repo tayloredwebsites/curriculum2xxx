8ea34dcb20d867052f18980e35cdeeefa652dbd8
esopp <esoppenheim@gmail.com>
2020-06-25 00:10:35 -0400

    Passing test/controllers/users_controller_test.rb

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -11,23 +11,27 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   include UserTestHelper
 
   setup do
-    testing_db_seeds
+    testing_db_tfv_seed
     setup_all_users
   end
 
-  test "public user should only see public pages" do
+  test "public user should only see sign_in page" do
     get root_path
-    assert_redirected_to root_path('bs')
+    assert_redirected_to new_user_session_path
     get trees_path
-    assert_response :success
+    assert_redirected_to new_user_session_path
     get uploads_path
-    assert_redirected_to new_user_session_path()
+    assert_redirected_to new_user_session_path
     get users_path
-    assert_redirected_to root_path('bs')
-    get edit_user_path('bs', @admin.id)
-    assert_redirected_to root_path('bs')
-    get edit_user_path('bs', @teacher.id)
-    assert_redirected_to root_path('bs')
+    assert_redirected_to new_user_session_path
+    get edit_user_path('en', @admin.id)
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_redirected_to new_user_session_path
+    get edit_user_path('en', @teacher.id)
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_redirected_to new_user_session_path
     get new_user_session_path
     assert_response :success
   end
@@ -35,66 +39,70 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
   test "unauth user should only see public pages" do
     sign_in @unauth
     get root_path
-    assert_redirected_to root_path('bs')
+    assert_redirected_to new_user_session_path
     get trees_path
-    assert_response :success
+    assert_redirected_to new_user_session_path
     get uploads_path
-    assert_redirected_to root_path('bs')
+    assert_redirected_to new_user_session_path
     get users_path
-    assert_redirected_to root_path('bs')
-    get edit_user_path('bs', @admin.id)
-    assert_redirected_to root_path('bs')
-    get edit_user_path('bs', @teacher.id)
-    assert_redirected_to root_path('bs')
+    assert_redirected_to new_user_session_path
+    get edit_user_path('en', @admin.id)
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_redirected_to new_user_session_path
+    get edit_user_path('en', @teacher.id)
+    assert_redirected_to root_path
+    follow_redirect!
+    assert_redirected_to new_user_session_path
     get new_user_session_path
-    assert_redirected_to root_path('bs')
+    assert_response :success
   end
 
   test "teacher should only see appropriate pages" do
     sign_in @teacher
     get root_path
-    assert_redirected_to root_path('bs')
+    assert_redirected_to root_path('en')
     get trees_path
     assert_response :success
     get uploads_path
-    assert_redirected_to root_path('bs')
+    assert_redirected_to root_path('en')
     get users_path
-    assert_redirected_to root_path('bs')
-    get edit_user_path('bs', @admin.id)
-    assert_redirected_to root_path('bs')
-    get edit_user_path('bs', @unauth.id)
-    assert_redirected_to root_path('bs')
-    get edit_user_path('bs', @teacher.id)
+    assert_redirected_to root_path('en')
+    get edit_user_path('en', @admin.id)
+    assert_redirected_to root_path('en')
+    get edit_user_path('en', @unauth.id)
+    assert_redirected_to root_path('en')
+    get edit_user_path('en', @teacher.id)
     assert_response :success
     get new_user_session_path
-    assert_redirected_to root_path('bs')
+    assert_redirected_to root_path('en')
   end
 
   test "admin user should see all pages" do
     sign_in @admin
     get root_path
-    assert_redirected_to root_path('bs')
+    assert_redirected_to root_path('en')
     get trees_path
     assert_response :success
     get uploads_path
     assert_response :success
     get users_path
     assert_response :success
-    get edit_user_path('bs', @admin.id)
+    get edit_user_path('en', @admin.id)
     assert_response :success
-    get edit_user_path('bs', @unauth.id)
+    get edit_user_path('en', @unauth.id)
     assert_response :success
-    get edit_user_path('bs', @teacher.id)
+    get edit_user_path('en', @teacher.id)
     assert_response :success
     get new_user_session_path
-    assert_redirected_to root_path('bs')
+    assert_redirected_to root_path('en')
   end
 
   test "admin user should be able to create, edit and view all users" do
     sign_in @admin
     get users_path
     assert_response :success
-    get edit_user_path('bs', @unauth.id)
+    get edit_user_path('en', @unauth.id)
     assert_response :success
     assert_equal '', assigns(:user).roles
     patch "/users/#{@unauth.id}", params: { user: { role_admin: 'on', role_teacher: 'on' } }
