caa07b0d4c0152a8c047e5488ba4be6a8b65a40d
esopp <esoppenheim@gmail.com>
2020-07-17 14:28:55 -0400

    save work on activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -5,4 +5,20 @@ class Activity < BaseRec
   belongs_to :dimension, optional: true
   has_many :resource_joins, as: :resourceable
   has_many :resources, through: :resource_joins
+
+
+  LOOKUP_TABLES = ['student_org', 'teach_strat']
+
+  def self.options_hash_and_transl_keys
+  	optionsHash = Hash.new { |h, k| h[k] = [] }
+  	translKeys = []
+  	LookupTablesOption.where(
+  		table_name: LOOKUP_TABLES
+  	).each do |l|
+      translKeys << l.lookup_translation_key
+  	  optionsHash[l.table_name] << l
+  	end
+  	return [optionsHash, translKeys]
+  end
+
 end
