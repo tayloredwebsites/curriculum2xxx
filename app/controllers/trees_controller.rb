d6a868a95f4eff08c190304af31fbd8ff12d2284
rapkat10 <aminrapkat@gmail.com>
2020-07-23 17:27:22 -0600

    fix tracker api call bug and pwd reset api call to sso

--- app/controllers/trees_controller.rb
+++ app/controllers/trees_controller.rb
@@ -834,12 +834,10 @@ class TreesController < ApplicationController
   end
 
   def show
-    body = { user: { email: current_user.email} }
     token = JWT.encode({email: current_user.email}, JWT_PASSWORD)
     response = HTTParty.get('http://localhost:3006/api/v1/tracker_pages', body: token).parsed_response
-    @sections = response['sections']
-    @sections = [] if @sections.nil?
-
+    response['sections'] ? @sections = response['sections'] : @sections = []
+    
     process_tree = (@tree[:depth] == @treeTypeRec[:outcome_depth])
     if process_tree
       @editMe = (params['editme'] == @tree.id.to_s) && can?(:update, @tree)
