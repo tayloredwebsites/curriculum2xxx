ae81239cb8d930d9b965f45d140e1c07472ca57d
rapkat10 <aminrapkat@gmail.com>
2020-07-23 08:43:32 -0600

    change port to 3005

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   # set default url option for devise.
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3005 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -55,5 +55,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.root_url = 'http://localhost:3000'
+  config.root_url = 'http://localhost:3005'
 end
